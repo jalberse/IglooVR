34c6c2798c753e747b8b518f84d3bedf74308d51
John Alberse <alberse.john@gmail.com>
2023-05-11 18:51:51 -0500

    Add Igloo Unity VR toolkit

---
+++ Assets/Igloo Toolkit/Plugins/extOSC/Scripts/Serialization/Packers/OSCPackerBounds.cs
@@ -0,0 +1,45 @@
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
+
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+namespace extOSC.Serialization.Packers
+{
+    public class OSCPackerBounds : OSCPacker<Bounds>
+    {
+        #region Protected Methods
+
+        protected override Bounds OSCValuesToValue(List<OSCValue> values, ref int start, Type type)
+        {
+            var center = new Vector3
+            {
+                x = values[start++].FloatValue,
+                y = values[start++].FloatValue,
+                z = values[start++].FloatValue
+            };
+
+            var size = new Vector3
+            {
+                x = values[start++].FloatValue,
+                y = values[start++].FloatValue,
+                z = values[start++].FloatValue
+            };
+
+            return new Bounds(center, size);
+        }
+
+        protected override void ValueToOSCValues(List<OSCValue> values, Bounds value)
+        {
+            values.Add(OSCValue.Float(value.center.x));
+            values.Add(OSCValue.Float(value.center.y));
+            values.Add(OSCValue.Float(value.center.z));
+            values.Add(OSCValue.Float(value.size.x));
+            values.Add(OSCValue.Float(value.size.y));
+            values.Add(OSCValue.Float(value.size.z));
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Igloo Toolkit/Plugins/extOSC/Scripts/Serialization/Packers/OSCPackerVector2.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
+
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+namespace extOSC.Serialization.Packers
+{
+    public class OSCPackerVector2 : OSCPacker<Vector2>
+    {
+        #region Protected Methods
+
+        protected override Vector2 OSCValuesToValue(List<OSCValue> values, ref int start, Type type)
+        {
+            var vector = new Vector2
+            {
+                x = values[start++].FloatValue,
+                y = values[start++].FloatValue
+            };
+
+            return vector;
+        }
+
+        protected override void ValueToOSCValues(List<OSCValue> values, Vector2 value)
+        {
+            values.Add(OSCValue.Float(value.x));
+            values.Add(OSCValue.Float(value.y));
+        }
+
+        #endregion
+    }
+}
